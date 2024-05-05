8ee62b11d95e791c2780c4b3ad394d1a4230dec0
ConquistadorCan <canmustafafatih@gmail.com>
2024-05-05 21:30:44 +0300

    Installed and integrated Vuetify library into project. Tried to understand rout functions and tried an example.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <!--Tried something-->
+        <RouterLink to="/fatih">Fatih</RouterLink>
       </nav>
     </div>
   </header>
