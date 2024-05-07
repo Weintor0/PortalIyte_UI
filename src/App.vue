a7f37db328d60271b63073b010fee6052c345454
ConquistadorCan <canmustafafatih@gmail.com>
2024-05-07 21:34:21 +0300

    deleted background-color and text color. main page separated four parts.

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,45 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <!--Tried something-->
-        <RouterLink to="/fatih">Fatih</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="top">TOP</div>
+  <div class="bottom">
+    <div class="left">left</div>
+    <div class="main">main</div>
+    <div class="right">right</div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.top {
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: yellow;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.bottom {
+  display: flex;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.left,
+.right,
+.main {
+  min-height: 75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.left,
+.right {
+  min-width: 15%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.main {
+  min-width: 70%;
+  background-color: orange;
 }
 </style>
