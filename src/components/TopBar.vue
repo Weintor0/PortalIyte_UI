f0da9ac957b64f6b3f458aee9038da699fe2be76
Mustafa Fatih Can <canmustafafatih@gmail.com>
2024-05-13 21:49:20 +0300

    preparing topbar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="top-bar">
+    <div class="logo">logo</div>
+    <v-text-field
+      class="search-bar"
+      label="Search"
+      variant="underlined"
+      clearable
+      prepend-inner-icon="mdi-delete"
+      density="compact"
+    ></v-text-field>
+    <div class="buttons">buttons</div>
+  </div>
+</template>
+
+<style scoped>
+.top-bar {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  height: 100%;
+  padding: 0.5rem 1.5rem;
+}
+
+.logo,
+.search-bar,
+.buttons {
+  margin-left: 3em;
+  margin-right: 3em;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import Fatih from '../views/Fatih.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    },
-    {
-      path: '/fatih',
-      name: 'fatih',
-      component: Fatih
-    }
-  ]
+  routes: []
 })
 
 export default router
