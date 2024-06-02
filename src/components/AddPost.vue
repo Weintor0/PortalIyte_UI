b44a8c2656d1ba909fad2903196c3863707e4d27
ConquistadorCan <canmustafafatih@gmail.com>
2024-06-02 19:33:21 +0300

    adjustments

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -1,131 +1,115 @@
 <template>
-    <div class="container">
-      <div class="add-header">Create Post</div>
-      <form @submit.prevent="submitPost">
-        <div>
-          <label for="topic" class="input-header">Topic</label>
-          <input type="text" id="topic" v-model="post.topic" />
-        </div>
-        <div>
-          <label for="title" class="input-header">Title</label>
-          <input type="text" id="title" v-model="post.title" />
-        </div>
-        <div>
-          <label for="text" class="input-header">Text</label>
-          <textarea id="text" v-model="post.text"></textarea>
-        </div>
-        <div class="bottom-container">
-            <div class="add-images">
-                <label>Add images</label>
-                <v-icon class="add-icon" @click="addImage">mdi-plus-circle</v-icon>
-            </div>
-            <button type="submit">Post</button>
-        </div>
-      </form>
+  <div class="add-header">Create Post</div>
+  <form @submit.prevent="submitPost">
+    <div>
+      <label for="topic" class="input-header">Topic</label>
+      <input type="text" id="topic" v-model="post.topic" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        post: {
-          topic: '',
-          title: '',
-          text: ''
-        }
-      };
-    },
-    methods: {
-      addImage() {
-        console.log('Image added', this.post);
-      },
-      submitPost() {
-        console.log('Post submitted:', this.post);
+    <div>
+      <label for="title" class="input-header">Title</label>
+      <input type="text" id="title" v-model="post.title" />
+    </div>
+    <div>
+      <label for="text" class="input-header">Text</label>
+      <textarea id="text" v-model="post.text"></textarea>
+    </div>
+    <div class="bottom-container">
+      <div class="add-images">
+        <label>Add images</label>
+        <v-icon class="add-icon" @click="addImage">mdi-plus-circle</v-icon>
+      </div>
+      <v-btn class="button" type="submit">Post</v-btn>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        topic: '',
+        title: '',
+        text: ''
       }
     }
-  };
-  </script>
-  
-  <style scoped>
-  .container {
-    height: 70%;
-    width: 70%;
-    padding: 20px;
-    border-radius: 10px;
-    margin: 0 auto;
-    background-color: rgba(128, 128, 128, 0.1);
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+  },
+  methods: {
+    addImage() {
+      console.log('Image added', this.post)
+    },
+    submitPost() {
+      console.log('Post submitted:', this.post)
+    }
   }
-  
-  .add-header{
-    text-align: start;
-    margin-bottom: 20px;
-    font-size: 1.5vw;
-    font-weight: bold;
-  }
+}
+</script>
 
-  .input-header{
-    font-size: 1vw;
-  }
-  
-  form div {
-    margin-bottom: 15px;
-  }
-  
-  label {
-    display: block;
-    margin-bottom: 5px;
-  }
-  
-  input[type="text"],
-  textarea {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-  
-  textarea {
-    resize: vertical;
-    height: 100px;
-  }
+<style scoped>
+.add-header {
+  text-align: start;
+  margin-bottom: 20px;
+  font-size: 1.5vw;
+  font-weight: bold;
+}
 
-  .bottom-container{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .add-images {
-    width: 20%;
-    display: flex;
-    font-size: 1vw;
-    flex-direction: column;
-  }
-  
-  .add-images button {
-    width: 30px;
-    height: 30px;
-    font-size: 20px;
-    margin-left: 10px;
-    cursor: pointer;
-  }
-  
-  button[type="submit"] {
-    padding: 10px 20px;
-    border-radius: 10px;
-    cursor: pointer;
-    width: 20%;
-    height: 50%;
-    text-align: center;
-    background-color: #D9D9D9;
-    font-size: 1vw;
-  }
+.input-header {
+  font-size: 1vw;
+}
 
-  .add-icon{
-    font-size: 2vw;
-  }
-  </style>
-  
+form div {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input[type='text'],
+textarea {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+textarea {
+  resize: vertical;
+  height: 100px;
+}
+
+.bottom-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.add-images {
+  width: 20%;
+  display: flex;
+  font-size: 1vw;
+  flex-direction: column;
+}
+
+.add-images button {
+  width: 30px;
+  height: 30px;
+  font-size: 20px;
+  margin-left: 10px;
+  cursor: pointer;
+}
+
+.button {
+  color: white;
+  background-color: #9a1220;
+  padding: 0.5em 1em;
+  margin: 0.5em 0;
+  border-radius: 5px;
+}
+
+.add-icon {
+  font-size: 2vw;
+}
+</style>
